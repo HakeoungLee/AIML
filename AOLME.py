51e517b82ba968e3f9a306d4def294c26a47339f
Marios S Pattichis <pattichis@gmail.com>
2025-06-09 14:58:04 +0300

    Update AOLME.py

--- AOLME.py
+++ AOLME.py
@@ -13,16 +13,14 @@ easy_messages = None #if you don't want traceback turn this on
 if easy_messages:
     sys.tracebacklimit = 0
     
-
-
 def grid_lines_on(width, height):
     print(width, height)
     fig1, ax = pyplot.subplots()
     ax.grid(linestyle='-',linewidth=0.5)
-    ax.set_xticks(np.linspace(0.5, height+0.5, height))
-    ax.set_xticklabels(range(0, height, 1))
-    ax.set_yticks(np.linspace(0.5, width+0.5, width))
-    ax.set_yticklabels(range(0, width, 1))
+    ax.set_xticks(np.linspace(0, height-1, height))
+    ax.set_xticklabels(range(0, height+1, 1))
+    ax.set_yticks(np.linspace(0, width-1, width))
+    ax.set_yticklabels(range(0, width+1, 1))
     return fig1,ax
 
 def check_input(img,which_lib):
